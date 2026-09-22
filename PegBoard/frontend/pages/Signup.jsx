e533e978d009883fe09eb21bac14ec0803e67e4a
u25245962-Keogh <u25245962@tuks.co.za>
2026-09-22 14:34:04 +0200

    completed signup page styling

--- PegBoard/frontend/pages/Signup.jsx
+++ PegBoard/frontend/pages/Signup.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../styles/Signup.css";
+
+import logo from "../assests/logo.png";
+import sculpture from "../assests/scultpure.png";
+import printing3D from "../assests/3Dprinting.png";
+import candle from "../assests/candle.png";
+import woodwork from "../assests/woodwork.png";
 
 function SignUp() {
   const [username, setUsername] = useState("");
@@ -66,36 +74,66 @@ function SignUp() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username</label>
-      <input
-        type="text"
-        placeholder="Username"
-        id="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
+    <>
+      <div id="signupDiv">
+        <h1 id="SUh1">Join Pegboard</h1>
+        <h2 id="SUh2">Sign up</h2>
+        <form onSubmit={handleSubmit} id="signupForm">
+          <div id="signupFormDiv">
+            <h3 id="SUh3">Sign up for pegboard</h3>
+            <label htmlFor="username" className="signupLabel">
+              Username
+            </label>
+            <input
+              type="text"
+              placeholder="user123"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="signupInput"
+            />
 
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        placeholder="email"
-        id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+            <label htmlFor="email" className="signupLabel">
+              Email
+            </label>
+            <input
+              type="text"
+              placeholder="example@mail.com"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="signupInput"
+            />
 
-      <label htmlFor="password">Password</label>      
-      <input
-        type="password"
-        placeholder="Password"
-        id="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">SignUp</button>
-      {error && <p>{error}</p>}
-    </form>
+            <label htmlFor="password" className="signupLabel">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="**********"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="signupInput"
+            />
+          </div>
+          <div id="signupButtonDiv">
+            <button type="submit" id="signupButton">
+              Sign up
+            </button>
+          </div>
+          {error && <p id="signupError">{error}</p>}
+        </form>
+        <p id="haveAccount">
+          already have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
+      <img src={logo} id="signUpLogo" />
+      <img src={sculpture} id="SUsculpture" className="splashImages" />
+      <img src={printing3D} id="SUprinting" className="splashImages" />
+      <img src={candle} id="SUcandle" className="splashImages" />
+      <img src={woodwork} id="SUwood" className="splashImages" />
+    </>
   );
 }
 export default SignUp;
